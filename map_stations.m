73f4ae022a7ad3b64505fbc6ad9a6e8d70887a43
martinvancop <martin.vancoppenolle@locean-ipsl.upmc.fr>
2020-05-11 15:24:11 +0200

    Station location data for mapping
    Implémentation des modifications de Bastien dans mon script (latitudes des stations et tout)

--- map_stations.m
+++ map_stations.m
@@ -1,15 +1,16 @@
 clear all, close all
 
+%----------------------------------------------------------------------------
+% Script test pour realiser une carte des stations (en utilisant une boucle)
+%----------------------------------------------------------------------------
 %
-%??????????????????????????????????????????????????
-% Script test pour réaliser une carte des stations
-%??????????????????????????????????????????????????
-% MartinV, 7 mai 2020
+% MartinV, 7 mai 2020, corrige par Bastien le 11 mai
 %
 
-figure('Color','w'); hold on;
+figure('Color','w'); 
+hold on;
 
-%--- réalisation de la carte
+% --- Realisation de la carte ---
 
 coast = load('coast'); % structure qui contient les cotes
 
@@ -18,47 +19,55 @@ coast = load('coast'); % structure qui contient les cotes
 % projection polaire stereo
 % axesm('stereo','Origin',[-90 0],'MapLatLimit',[-90 -60]) % definit les axes de la carte
 
-% projection stéréographique de Gall (localisée pour une région)
-axesm('gstereo','MapLonLimit', [-90, 20.],'MapLatLimit',[-80 -60]) % definit les axes de la carte
+% projection stereographique de Gall (localisee pour une region)
+axesm('gstereo','MapLonLimit', [-65, 5],'MapLatLimit',[-80 -60]) % definit les axes de la carte
   
 framem on % affiche le cadre
-axis on   % affiche les axes
-gridm on  % affiche la grille
-mlabel on % affiche les étiquettes des méridiens
-plabel on % affiche les étiquettes des parallèles
+axis   off  % affiche les axes
+gridm  on  % affiche la grille
+mlabel on % affiche les etiquettes des meridiens
+plabel on % affiche les etiquettes des paralleles
 
 geoshow(coast.lat,coast.long, 'DisplayType','polygon', 'FaceColor', [135 206 250]/255.) % affiche le continent antarctique
 
-%--- ajout des données des stations
+% Ajout des donnees des stations (latitude / longitude)
+N_sta = 18
+lat_sta = [-69.06889, -69.1024863, -70.1951665, -70.28556, -70.2991597, -70.33583, -70.3516683, -70.38528, -70.50056, -70.50111, -70.63361, -70.4590423, -71.02668, -71.2415445, -67.6691357, -67.5325338, -65.20111, -65.5224318 ] % donnÃ©es latitude (ordre chronologique)
+lon_sta = [-0.09944444, -0.0379608, -11.248467, -9.985833, -10.0443703, -8.935278, -8.975854, -8.752222, -8.185,  -9.166667, -9.901667, -8.3512282, -13.4930235, -19.6462462 ,-46.3136623, -46.3817188, -45.935, -46.113281 ] % donnÃ©es longitude (ordre chronologique)
 
-N_sta = 2
+sta_type = [ 1, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 2 ] % 1 pour L-arm, 2 pour ROV 
 
-lat_sta = [ -69.06889, -65 ] % ici tu dois remplacer par tes donnees lat / lon
-lon_sta = [ -0.09944444, -15 ] % ici tu dois remplacer par tes données lat / lon
-
-sta_type = [ 1, 2 ] % 1 pour ROV, 2 pour L-arm % ici faut remplir aussi
-
-is_tag   = [ 1, 0 ] % 1 si on a de la donnee tag, 0 si pas % ici faut remplir aussi
+is_tag   = [ 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ] % 1 si de la donnee tag est presente, 0 dans le cas contraire
 
 for i_sta = 1:N_sta
 
-    % stations de type 'ROV', avec du tag
-    if ( sta_type(i_sta) == 1 & is_tag(i_sta) == 1) 
-       scatterm( lat_sta(i_sta), lon_sta(i_sta), 30, 'ksq', 'MarkerFaceColor', 'k')
+    % stations de type ROV, avec du tag (losange rouge plein)
+    if ( sta_type(i_sta) == 2 & is_tag(i_sta) == 1  ) % losange rouge
+        H(1) = scatterm( lat_sta(i_sta), lon_sta(i_sta), 40, 'd', 'filled', 'MarkerFaceColor', 'r') 
     end
     
-    % stations de type 'ROV', sans tag
-    % [ ajouter le code ici ]
-    
-    % stations de type 'L-arm', avec tag
-    % [ ajouter le code ici ]
-    
-    % stations de type 'L-arm', sans tag
-    if ( sta_type(i_sta) == 2 & is_tag(i_sta) == 0) 
-       scatterm( lat_sta(i_sta), lon_sta(i_sta), 30, 'bo' )
+    % stations de type ROV, sans tag (losange rouge vide)
+    if ( sta_type(i_sta) == 2 & is_tag(i_sta) == 0 )  
+        H(2) = scatterm( lat_sta(i_sta), lon_sta(i_sta), 80, 'd', 'r') 
     end
     
+    % stations de type L-arm, avec tag (rond noir plein)
+    if ( sta_type(i_sta) == 1 & is_tag(i_sta) == 1 )
+        H(3) = scatterm( lat_sta(i_sta), lon_sta(i_sta), 40, 'o', 'filled', 'MarkerFaceColor', 'k')
+    end 
     
+    % stations de type L-arm, sans tag (rond noir vide)
+    if ( sta_type(i_sta) == 1 & is_tag(i_sta) == 0 ) 
+       H(4) = scatterm( lat_sta(i_sta), lon_sta(i_sta), 80, 'o', 'k')
+    end
     
 end
+
+% Axe, titre et legende
+xlabel( 'Longitude', 'color', 'k') % changer la couleur du titre en abscisse
+ylabel( 'Latitude' , 'color', 'k') % changer la couleur du titre en ordonnee
+title( ['PS117 stations'], 'FontSize', 16)
+%legend (H, {'\fontsize {11}Ice (L-Arm) Station', '\fontsize {11}ROV Station', '\fontsize {11}Les symboles vides correpondent Ã  des stations ne prÃ©sentant pas de donnÃ©es issues du tag',''}, 'Location', 'southoutside')
+legend ([H(1) H(3)], {'L-ARM', 'ROV'})
+%legend ('boxoff') % Pas de cadre autour de la legende
 
